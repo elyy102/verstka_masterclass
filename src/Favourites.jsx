57b2fddc33242cafbc822672b463e6324329e4d8
elyy102 <103173379+elyy102@users.noreply.github.com>
2024-02-27 20:43:22 +0500

    2

---
+++ src/Favourites.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Favourites = () => {
+  return (
+    <div className="wrapper">
+        <h1 className="title">Хочу сходить</h1>
+    </div>
+  )
+}
+
+export default Favourites
---
+++ src/My_mk.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Favourites = () => {
+  return (
+    <div className="wrapper">
+        <h1 className="title">Мои мастер-классы</h1>
+    </div>
+  )
+}
+
+export default Favourites
